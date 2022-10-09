e2007e966ac22f2c92fbfea8d54e2f7581b956ae
Aartimehr <106958470+Aartimehr@users.noreply.github.com>
2022-10-09 22:44:04 +0530

    Update breaking the records.cpp

--- Assignment 4/breaking the records.cpp
+++ Assignment 4/breaking the records.cpp
@@ -1,27 +1,122 @@
-#include <bits/stdc++.h>
+include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    int n,m=0,p=0;
-    cin>>n;
-    int arr[n]; //arr={3,4,6,2,4,6}
-    for(int i=0;i<n;i++) cin>>arr[i];
-    int max=arr[0];
-    int min=arr[0];
-    //for maximum score
-    for(int i=1;i<n;i++){
-        if(arr[i] > max){ 
-            max = arr[i];
-            m++;
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+vector<int> breakingRecords(vector<int> scores) {
+    int max = scores[0];
+    int min = scores[0];
+    int h=0,l=0;
+    vector<int> ans;
+    for(int i=0;i<scores.size();i++){
+        if(max<scores[i]) {
+            max=scores[i];
+            h++;
         }
-        else if (arr[i]<min){ 
-            min= arr[i];
-            p++;
-            }
+        else if (min>scores[i]){
+            min=scores[i];
+            l++; 
+        }
         else continue;
     }
+    ans.push_back(h);
+    ans.push_back(l);
     
-    cout<<m<<" "<<p<<endl;
+    return ans;
+    
+    
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split(rtrim(scores_temp_temp));
+
+    vector<int> scores(n);
+
+    for (int i = 0; i < n; i++) {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    vector<int> result = breakingRecords(scores);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
     return 0;
 }
-Fo
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+
+
+    
