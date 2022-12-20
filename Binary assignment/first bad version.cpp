947df212917b64ec8a335647fad9cf397eb03749
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-12-20 13:04:17 +0530

    Update first bad version.cpp

--- Binary assignment/first bad version.cpp
+++ Binary assignment/first bad version.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int guessNumber(int n) {
-        long int high = n-1;
+    int firstBadVersion(int n) {
+        long int high = n;
         long int low = 1;
         long int mid;
-        while(low<=high){
-            mid=(high+low)/2;
-            if(guess(mid)==0) return mid;
-            else if(guess(mid)<0) high=mid-1;
-            else low = mid+1;
+        while(low<high){
+            mid=low+(high-low)/2;
+            if (isBadVersion(mid))high = mid;
+            else low=mid+1;
         }
-        if(guess(high)==0) return high;
-        else return low; 
+        return low;
+    
     }
 };
