da44323f3a96d71f9f20bfb7dac51dc1e004350b
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-01-17 23:10:27 +0530

    Update find all duplicates in any array.cpp

--- Assignment hashing.cpp/find all duplicates in any array.cpp
+++ Assignment hashing.cpp/find all duplicates in any array.cpp
@@ -4,18 +4,17 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>duplicates;
             map<int,int>mp;
-            for(int i=1;i<nums.size();i++)
+            for(int i=0;i<nums.size();i++)
             {
                 mp[nums[i]]++;
             }
             for(auto i : mp)
             { 
-                 mp[i.first]++;
-               if(i.second==2)
+               if(i.second>1)
                {
                    duplicates.push_back(i.first);
                    
-               }
+ }
                else
                cout<<" ";
             }
@@ -23,3 +22,6 @@ public:
     }
 
 };
+
+
+//Firstly frequency has been taken 
