3066664ef2da9c8e4c2b89aef275afe71116c73a
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-01-26 13:46:16 +0530

    Create preorder tree hackerrank.cpp

---
+++ Assignment 10 tree/preorder tree hackerrank.cpp
@@ -0,0 +1,10 @@
+void preOrder(Node *root) {
+            if(root==NULL)
+            {
+                return;
+            }
+            cout<<root->data<<" ";
+            preOrder(root->left);
+            preOrder(root->right);
+            
+    }
